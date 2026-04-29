56548f12a46e7daf0ada509db19a57b5474a4c28
sasgroups <saiganesh@sasgroups.net>
2026-04-29 13:19:47 +0530

    Restructured frontend into client folder

--- src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-dark"></div>
-    </div>
-  );
-}
--- src/config.js
+++
@@ -1,2 +0,0 @@
-const API_BASE = 'http://localhost:5000/api';
-export default API_BASE;
